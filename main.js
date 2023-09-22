9d527dbf2079e6512f1769d5f74d6c6016bb1632
mdrahed2023 <135752931+mdrahed2023@users.noreply.github.com>
2023-09-22 11:05:43 +0600

    change hello

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const notiCancel = document.querySelector(".noti-cancel");
 const notification = document.querySelector(".notification");
 const sidebarLi = document.querySelectorAll("#sidebar ul li i");
 const tabs = document.querySelectorAll(".tabs-item");
+const orderCard = document.querySelectorAll(".order-card");
+const order = document.querySelector(".order");
+
+const orderTouch = document.querySelector(".order-visible");
 
 showNoti.addEventListener("click", () => {
     notification.style.right = "0";
@@ -32,3 +36,25 @@ sidebarLi.forEach((e, ind) => {
         tabs[ind].classList.add("active")
     })
 })
+
+orderCard.forEach(e => {
+    e.addEventListener("click", () => {
+
+        orderCard.forEach(es => {
+            es.style.height = "100px";
+            es.querySelector(".o-bottombar").style.display = "none";
+            
+        })
+
+        console.log("click");
+        e.style.height = "500px";
+        e.querySelector(".o-bottombar").style.display = "flex";
+    });
+
+    
+})
+
+
+
+
+
